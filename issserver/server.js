22cead31eb8af63e123d837ef32b722694bca573
Thomas Sarlandie <thomas@sarlandie.net>
2013-10-05 04:36:22 +0000

    Get server working on node 0.6

--- issserver/server.js
+++ issserver/server.js
@@ -3,13 +3,17 @@ var http = require('http');
 
 var app = express();
 
-app.get('/iss/', function(req, res) {
+app.post('/iss/', function(req, res) {
   console.log("got request");
 
   console.log(JSON.stringify(req.query));
 
   // Get next ISS passes over Boston
-  var http_request = http.get('http://api.open-notify.org/iss-pass.json?lat=42.3646&lon=-71.1028', function(http_response) {
+  var options = {
+    hostname: 'api.open-notify.org',
+    path: '/iss-pass.json?lat=42.3646&lon=-71.1028'
+  };
+  var http_request = http.request(options, function(http_response) {
     console.log("Status: " + http_response.statusCode);
 
     http_response.on('data', function(data) {
@@ -18,7 +22,7 @@ app.get('/iss/', function(req, res) {
       var next_passage = issdata.response[0].risetime;
 
       // Adjust UTC Time to Boston time
-      next_passage += 5 * 3600;
+      next_passage -= 4 * 3600;
 
       var response = {
         0: next_passage
@@ -27,10 +31,11 @@ app.get('/iss/', function(req, res) {
       res.end(JSON.stringify(response));
     });
   });
-  http_request.on('error', function() {
-    console.log("HTTP Request failed.");
+  http_request.on('error', function(e) {
+    console.log("HTTP Request failed: " + e.message);
     res.end('{ "0": -1 } ');
   });
+  http_request.end();
 
 });
 
